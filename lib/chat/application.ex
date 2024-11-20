701ac083250e0534ed155e0884666ed0623c742b
Spencer Olson <solson@instructure.com>
2024-11-20 13:04:50 -0600

    moar cleanup/remove unused code

--- lib/chat/application.ex
+++ lib/chat/application.ex
@@ -1,6 +1,4 @@
 defmodule Chat.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -9,12 +7,8 @@ defmodule Chat.Application do
   def start(_type, _args) do
     children = [
       Chat.Server
-      # Starts a worker by calling: Chat.Worker.start_link(arg)
-      # {Chat.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Chat.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- test/chat_test.exs
+++ test/chat_test.exs
@@ -1,8 +1,4 @@
 defmodule ChatTest do
   use ExUnit.Case
   doctest Chat
-
-  test "greets the world" do
-    assert Chat.hello() == :world
-  end
 end
