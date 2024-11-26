167917c8acf09a5b86bddf8bcc0152dcc8ddf586
Spencer Olson <solson@instructure.com>
2024-11-26 11:59:26 -0600

    add escript

--- lib/chat.ex
+++ lib/chat.ex
@@ -1,6 +1,8 @@
 defmodule Chat do
   alias Chat.{Clustering, Input, Screen, Server}
 
+  def main(_), do: start()
+
   def start do
     Clustering.setup()
     Screen.setup()
--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Chat.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: []
+      deps: [],
+      escript: escript()
     ]
   end
 
@@ -16,5 +17,9 @@ defmodule Chat.MixProject do
       extra_applications: [:logger],
       mod: {Chat.Application, []}
     ]
+  end
+
+  defp escript do
+    [main_module: Chat, strip_beams: true, embed_elixir: true]
   end
 end
