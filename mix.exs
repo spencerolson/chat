05268e93405b2c9cfd74d28e16006b06a128f553
Spencer Olson <solson@instructure.com>
2025-06-27 16:06:37 -0500

    version: 0.1.0 -> 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chat.MixProject do
   def project do
     [
       app: :chat,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: [],
